56a2ad838466b46aaee31d6ae933529db7c42fe1
Wojciech Decker <wdk@touk.pl>
2017-11-04 17:13:01 +0100

    Dodano bibliografię i opis korpusu

--- buildThesis.sh
+++ buildThesis.sh
@@ -5,7 +5,9 @@ THESIS_DIR=LATEX_dyplom
 THESIS_NAME=praca_dyplomowa
 
 cd $THESIS_DIR
-pdflatex $THESIS_NAME.tex
+pdflatex $THESIS_NAME
+bibtex $THESIS_NAME
+pdflatex $THESIS_NAME
 cp $THESIS_NAME.pdf /tmp/
 
 
