dcf502ee9ec462be98d761f82d7abd8cff25c4f9
Wojciech Decker <wdk@touk.pl>
2017-12-02 13:29:37 +0100

    Dodano tabelę wyników celności do pracy

--- aplikacja_py2/feature_selection.py
+++ aplikacja_py2/feature_selection.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-import csv
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.datasets import load_iris
-import pandas as pd
-from mlxtend.feature_selection import SequentialFeatureSelector as SFS
-import csv
 import numpy as np
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import scale
-from sklearn.mixture import GaussianMixture
-from sklearn.svm import SVC
+from pylatex import Tabular, Document
+
 import agh
 
-d=agh.load_file('features_analysis-cc.csv')
-X = d.data
-y=d.target
+bunch = agh.load_file('features_analysis-cc.csv')
+X = bunch.data
+y = bunch.target
 X_scaled = scale(X)
 
 # iris = load_iris()
@@ -24,27 +21,56 @@ X_scaled = scale(X)
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.33, random_state=1)
 
-classificator = KNeighborsClassifier(n_neighbors=len(d.target_names))
+classificator = KNeighborsClassifier(n_neighbors=len(bunch.target_names))
 # classificator = GaussianMixture(n_components=len(d.target_names), covariance_type='full')
 # classificator=SVC()
 # najlepsze wyniki dla KNN !!?
 
 sfs1 = SFS(classificator,
-          k_features=5,
-          forward=True,
-          floating=False,
-          scoring='accuracy',
-          cv=4,
-          n_jobs=-1)
+           k_features=5,
+           forward=True,
+           floating=False,
+           scoring='accuracy',
+           cv=4,
+           n_jobs=-1)
 sfs1 = sfs1.fit(X_train, y_train)
 print('Selected features:', sfs1.k_feature_idx_)
 
 X_train_sfs = sfs1.transform(X_train)
 X_test_sfs = sfs1.transform(X_test)
+
 # Fit the estimator using the new feature subset
 # and make a prediction on the test data
 classificator.fit(X_train_sfs, y_train)
 y_pred = classificator.predict(X_test_sfs)
+
+# tabela skuteczności
+accuaricyMatrix = np.zeros((len(bunch.target_names), len(bunch.target_names)), dtype=np.int64)
+for n in range(len(y_pred)):
+    accuaricyMatrix[y_pred[n], y_test[n]] += 1
+
+
+# tabela-wstawka
+def generate_table(names, data, corner=''):
+    lists = data.tolist()
+    table_schema = '|l' + '|c' * len(names) + '|'
+    table = Tabular(table_schema)
+    table.add_hline()
+    table.add_row([corner] + names)
+    table.add_hline()
+    for n in range(len(names)):
+        table.add_row([names[n]] + lists[n])
+        table.add_hline()
+    return table
+def saveLatex(obj, filename):
+    with open(filename, "wb") as f:
+        f.write(obj.dumps())
+emotions = ['irytacja','stan neutralny','radosc','smutek','strach','zdziwienie','zlosc']
+
+saveLatex(
+    generate_table(emotions, accuaricyMatrix,'pred\\exp'),
+    'share/accuaricy_table.tex'
+)
 
 # Compute the accuracy of the prediction
 acc = float((y_test == y_pred).sum()) / y_pred.shape[0]
