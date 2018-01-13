786bb2667971df3db898ce911c17c94f7f76be23
Wojciech Decker <wdk@touk.pl>
2018-01-13 23:30:31 +0100

    PCA

--- buildThesis.sh
+++ buildThesis.sh
@@ -5,6 +5,7 @@ THESIS_DIR=LATEX_dyplom
 THESIS_NAME=praca_dyplomowa
 make -C $THESIS_DIR clean
 make -C $THESIS_DIR
+cp $THESIS_DIR/$THESIS_NAME.pdf /tmp/$THESIS_NAME-$(date +%Y%m%d)-$(git rev-parse --short HEAD).pdf
 cp $THESIS_DIR/$THESIS_NAME.pdf /tmp/
 
 
