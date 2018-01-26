8318c9fc476ae5b197f54d8ae7845cddee5beef2
Wojciech Decker <wdk@touk.pl>
2018-01-26 01:02:00 +0100

    tabela celności dla mlp i pca

--- aplikacja_py2/latex_tables.py
+++ aplikacja_py2/latex_tables.py
@@ -1,4 +1,5 @@
 import json
+from itertools import product
 
 from emo.prezentation import saveLatex, generate_table, map_to_table
 import numpy as np
@@ -28,16 +29,22 @@ save_latex_table(filename, left_corner, r)
 
 # accuracy of MLP and PCA for seven emotions
 r = {}
-for c in data['seven']:
-    if classification['selector'] is 'pca' and c['classifier'] is 'mlp':
+emotions = ['irytacja', 'stan neutralny', 'radosc', 'smutek', 'strach', 'zdziwienie', 'zlosc']
+for p in product(emotions,emotions):
+    r[p]=0
+for classification in data['seven']:
+    if classification['selector'] == 'pca' and classification['classifier'] == 'mlp':
         predicted = np.array(classification['predicted'])
         expected = np.array(classification['expected'])
-        r[(classification['selector'], classification['classifier'])] = \
-            float((predicted == expected).sum()) / expected.shape[0] * 100
-filename = 'share/accuaricy_c_s_table.tex'
+        for n in range(expected.shape[0]):
+            predicted_n = predicted[n]
+            expected_n = expected[n]
+            r[(emotions[predicted_n],emotions[expected_n])] += 1
+        # r[(classification['selector'], classification['classifier'])] = \
+        #     float((predicted == expected).sum()) / expected.shape[0] * 100
+filename = 'share/classification_pca_mpl_table.tex'
 left_corner = 'pred\\exp'
 save_latex_table(filename, left_corner, r)
-emotions = ['irytacja', 'stan neutralny', 'radosc', 'smutek', 'strach', 'zdziwienie', 'zlosc']
 accuaricyMatrix = [[1] * 7] * 7
 saveLatex(
     generate_table((emotions, emotions), accuaricyMatrix, 'pred\\exp'),
